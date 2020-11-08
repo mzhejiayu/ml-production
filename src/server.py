bc458ceff0bedbb892061c11f999441f88993dc1
Zhejiayu MA <ma.zhejiayu@easybroadcast.fr>
2020-11-08 20:49:25 +0100

    Server scripts.

--- src/server.py
+++ src/server.py
@@ -1,5 +1,11 @@
-from flask import Flask, request
-from jsonschema import validate, ValidationError
+import json
+import os
+import requests
+from flask import Flask, current_app, g, has_app_context, jsonify, request, abort
+from joblib import load
+from jsonschema import ValidationError, validate
+
+from .encoder import Encoder
 
 from .cli import data_cli
 
@@ -10,6 +16,14 @@ encode the request into tensorflow api.
 
 app = Flask(__name__)
 app.config.from_pyfile("settings.cfg")
+
+# Update the configuration with environments starting with MLP_
+for key in os.environ.keys():
+    if key.startswith("MLP_"):
+        env_name = key.split("MLP_")[1]
+        app.config[env_name] = os.environ.get(key)
+
+
 app.cli.add_command(data_cli)
 
 request_schema = {
@@ -17,6 +31,15 @@ request_schema = {
     "default": [],
     "items": {"type": "array", "default": [], "items": {"type": "string"}},
 }
+
+
+def get_encoder():
+    if has_app_context():
+        path = current_app.config["PIPE_PATH"]
+        if "encoder" not in g:
+            pipe = load(path)
+            g.encoder = Encoder(pipe)
+        return g.encoder
 
 
 @app.errorhandler(ValidationError)
@@ -29,11 +52,21 @@ def prediction():
     data = request.get_json()
     validate(data, request_schema)
 
-    # encode
-    # request api from tensorflow
+    matrix = get_encoder().encode(data).tolist()
 
-    return "Ok"
+    res = requests.post(
+        current_app.config["TFS_URL"] + "/v1/models/tp_pred:predict",
+        data=json.dumps({"instances": matrix}),
+    )
+
+    if res.status_code != 200:
+        abort(res.status_code, description=f"{res}")
+    else:
+        return jsonify(res.json())
 
 
-if __name__ == "__main__":
-    app.run()
+def tfs_healthcheck():
+    res = requests.get(app.config["TFS_URL"], timeout=0.01)
+
+
+tfs_healthcheck()
---
+++ src/tests/__init__.py
@@ -0,0 +1,10 @@
+import pytest
+
+__doc__ = """
+The integration test of the program consists of 
+- generating the data.
+- providing the saved_model
+- load the model
+- provide test input raw data
+- verify the output data
+"""
